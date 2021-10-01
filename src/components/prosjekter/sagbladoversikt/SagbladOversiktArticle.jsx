edeef4c0b6cb15f8c3c7f329f7fd73d45f13315d
KjetilOverby <38867894+KjetilOverby@users.noreply.github.com>
2021-10-01 12:39:58 +0200

    Update SagbladOversiktArticle.jsx

--- src/components/prosjekter/sagbladoversikt/SagbladOversiktArticle.jsx
+++ src/components/prosjekter/sagbladoversikt/SagbladOversiktArticle.jsx
@@ -9,7 +9,7 @@ const SagbladOversiktArticle = () => {
           Jeg bygger en ny versjon av sagbladregister. Denne gangen er ting
           forbedret og er sikrere mot at uautoriserte får tilgang til data.
           Prosjektet bruker fortsatt den samme databasen så data trenger man
-          ikke å legge inn på nytt. test trying again
+          ikke å legge inn på nytt. test trying again from github
         </p>
       </div>
       <style jsx>
