f6053f8fda25bc5e2b86eb7b5dcec98ea8321d2a
Kjetil Overby <kjeoever@msn.com>
2021-10-01 11:46:02 +0200

    new test

--- src/components/prosjekter/sagbladoversikt/SagbladOversiktArticle.jsx
+++ src/components/prosjekter/sagbladoversikt/SagbladOversiktArticle.jsx
@@ -9,12 +9,13 @@ const SagbladOversiktArticle = () => {
           Jeg bygger en ny versjon av sagbladregister. Denne gangen er ting
           forbedret og er sikrere mot at uautoriserte får tilgang til data.
           Prosjektet bruker fortsatt den samme databasen så data trenger man
-          ikke å legge inn på nytt. test
+          ikke å legge inn på nytt. test trying again
         </p>
       </div>
       <style jsx>
         {`
           .container {
+            display: grid
           }
         `}
       </style>
