7b2f9003af6d689face442f33485f3a0372ed5f7
Kjetil Overby <kjeoever@msn.com>
2021-10-01 12:51:59 +0200

    again

--- src/components/prosjekter/sagbladoversikt/SagbladOversiktArticle.jsx
+++ src/components/prosjekter/sagbladoversikt/SagbladOversiktArticle.jsx
@@ -9,7 +9,7 @@ const SagbladOversiktArticle = () => {
           Jeg bygger en ny versjon av sagbladregister. Denne gangen er ting
           forbedret og er sikrere mot at uautoriserte får tilgang til data.
           Prosjektet bruker fortsatt den samme databasen så data trenger man
-          ikke å legge inn på nytt. now
+          ikke å legge inn på nytt. now and again
         </p>
       </div>
       <style jsx>
