fcdd7813c096e1ee26c01392ee8fb3b2a2d017af
Kjetil  Overby <kjeoever@msn.com>
2021-10-01 11:31:24 +0200

    test

--- src/components/prosjekter/sagbladregister2/Sagbladregister2Article.jsx
+++ src/components/prosjekter/sagbladregister2/Sagbladregister2Article.jsx
@@ -7,7 +7,9 @@ const Sabladregister2Article = () => {
         <h1 className="header mb">Sagbladregister 2</h1>
         <p>
           Jeg bygger en ny versjon av sagbladregister. Denne gangen er ting
-          forbedret og er sikrere mot at uautoriserte får tilgang til data. Prosjektet bruker fortsatt den samme databasen så data trenger man ikke å legge inn på nytt.
+          forbedret og er sikrere mot at uautoriserte får tilgang til data.
+          Prosjektet bruker fortsatt den samme databasen så data trenger man
+          ikke å legge inn på nytt. test
         </p>
       </div>
       <style jsx>
