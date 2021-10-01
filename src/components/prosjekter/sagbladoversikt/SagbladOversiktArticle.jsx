ceaae9f82279843a889999e889f92f7cc5c73a1c
Kjetil  Overby <kjeoever@msn.com>
2021-10-01 12:51:14 +0200

    newtest

--- src/components/prosjekter/sagbladoversikt/SagbladOversiktArticle.jsx
+++ src/components/prosjekter/sagbladoversikt/SagbladOversiktArticle.jsx
@@ -4,18 +4,18 @@ const SagbladOversiktArticle = () => {
   return (
     <>
       <div className="container">
-      <h1 className="header mb">Sagbladregister 2</h1>
-      <p>
+        <h1 className="header mb">Sagbladregister 2</h1>
+        <p>
           Jeg bygger en ny versjon av sagbladregister. Denne gangen er ting
           forbedret og er sikrere mot at uautoriserte får tilgang til data.
           Prosjektet bruker fortsatt den samme databasen så data trenger man
-          ikke å legge inn på nytt. test trying again from github
+          ikke å legge inn på nytt. now
         </p>
       </div>
       <style jsx>
         {`
           .container {
-            display: grid
+            display: grid;
           }
         `}
       </style>
--- src/components/prosjekter/sagbladregister2/Sagbladregister2Article.jsx
+++ src/components/prosjekter/sagbladregister2/Sagbladregister2Article.jsx
@@ -5,12 +5,7 @@ const Sabladregister2Article = () => {
     <>
       <div className="container">
         <h1 className="header mb">Sagbladregister 2</h1>
-        <p>
-          Jeg bygger en ny versjon av sagbladregister. Denne gangen er ting
-          forbedret og er sikrere mot at uautoriserte får tilgang til data.
-          Prosjektet bruker fortsatt den samme databasen så data trenger man
-          ikke å legge inn på nytt. test testing
-        </p>
+        <p>sagbladregister 2</p>
       </div>
       <style jsx>
         {`
