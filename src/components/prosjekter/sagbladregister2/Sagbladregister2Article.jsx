70050ca4d22312299e794335807e547f1fbc68a5
Kjetil  Overby <kjeoever@msn.com>
2021-10-01 10:29:45 +0200

    sagbladregister 2

--- src/components/prosjekter/sagbladregister2/Sagbladregister2Article.jsx
+++ src/components/prosjekter/sagbladregister2/Sagbladregister2Article.jsx
@@ -4,7 +4,11 @@ const Sabladregister2Article = () => {
   return (
     <>
       <div className="container">
-        <h1 className="header">Sagbladregister 2</h1>
+        <h1 className="header mb">Sagbladregister 2</h1>
+        <p>
+          Jeg bygger en ny versjon av sagbladregister. Denne gangen er ting
+          forbedret og er sikrere mot at uautoriserte får tilgang til data.
+        </p>
       </div>
       <style jsx>
         {`
