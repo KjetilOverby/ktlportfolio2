07719db4bd1cf54a1c839ee9525efce2f4b7b13f
Kjetil Overby <kjeoever@msn.com>
2021-10-01 11:34:01 +0200

    new test

--- src/components/prosjekter/sagbladoversikt/SagbladOversiktArticle.jsx
+++ src/components/prosjekter/sagbladoversikt/SagbladOversiktArticle.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const SagbladOversiktArticle = () => {
   return (
     <>
-      <div className="container">Hello world</div>
+      <div className="container">
+      <h1 className="header mb">Sagbladregister 2</h1>
+      <p>
+          Jeg bygger en ny versjon av sagbladregister. Denne gangen er ting
+          forbedret og er sikrere mot at uautoriserte får tilgang til data.
+          Prosjektet bruker fortsatt den samme databasen så data trenger man
+          ikke å legge inn på nytt. test
+        </p>
+      </div>
       <style jsx>
         {`
           .container {
